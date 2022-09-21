93b55ada0752dda2bbbf8d15811e87c96a7ecf90
ronewa <ronewamavhungu@gmail.com>
2022-09-21 11:15:10 +0200

    adding issue-report

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IssueListComponent
+    IssueListComponent,
+    IssueReportComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
